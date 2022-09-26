98ea7e489a0a5b5841193b2f213e4120785f453a
SicongZhang <zhang.sico@northeastern.edu>
2022-09-26 14:51:28 -0700

    add modal and new button

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 import Header from './components/Header';
 import Input from './components/Input';
+import {useState} from 'react';
 
 export default function App() {
 
-    const onTextAdd = function (newText) {console.log(newText)}
+    const onTextAdd = function (newText) {console.log(newText); setModalVisible(false);}
+    const onCancel = function () {setModalVisible(false)}
     const name = "My App"
+    const [modalVisible, setModalVisible] = useState(false)
+    
 
   return (
     <View style={styles.container}>
      <Header appName = {name}/>
-     <Input onAdd = {onTextAdd}/>
+     <Button title="Add a Goal" onPress={() => setModalVisible(true)}/>
+     <Input onAdd = {onTextAdd} modal={modalVisible} onCancel = {onCancel}/>
       <StatusBar style="auto" />
 
     </View>
--- components/Input.js
+++ components/Input.js
@@ -1,14 +1,27 @@
-import { TextInput, View, Text, Button} from 'react-native';
+import { TextInput, View, Text, Button, Modal, StyleSheet} from 'react-native';
 import {useState} from 'react';
-export default function Input(props){
+
+export default function Input({onAdd, modal, onCancel}){
   const [text, setText] = useState("")
   return (
-      <View>
-      <TextInput
+    <Modal visible={modal}>
+      <View style={styles.container}>
+        <TextInput
           onChangeText = {(newText) => {setText(newText)}}
           value = {text}
           placeholder = "Please type your input here."/>
-          <Button title="Press me" onPress={() => {props.onAdd(text); setText("");}}/>
-          </View>
+        <Button title="Press me" onPress={() => {onAdd(text); setText("");}}/>
+        <Button title="Cancel" onPress={onCancel}/>
+      </View>
+    </Modal>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
