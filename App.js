731a8121ffa69fb15a79ee3a92883d4e34610e32
SicongZhang <zhang.sico@northeastern.edu>
2022-10-03 14:46:52 -0700

    use array.map to impletment a list

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TextInput, Button, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+} from "react-native";
 import Header from "./components/Header";
 import Input from "./components/Input";
 import { useState } from "react";
 
 export default function App() {
-  const [goals, setGoals] = useState([])
+  const [goals, setGoals] = useState([]);
   const onTextAdd = function (newText) {
-    const newGoal = {text:newText, key:Math.random()};
-    setGoals((goals) => {return [...goals, newGoal];});
+    const newGoal = { text: newText, key: Math.random() };
+    setGoals((goals) => {
+      return [...goals, newGoal];
+    });
     console.log(goals);
     setModalVisible(false);
   };
@@ -21,12 +30,18 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.topContainer}>
-      <Header appName={name} />
-      <Button title="Add a Goal" onPress={() => setModalVisible(true)} />
+        <Header appName={name} />
+        <Button title="Add a Goal" onPress={() => setModalVisible(true)} />
       </View>
       <View style={styles.bottomContainer}>
-        <View style={styles.textContainer}>
-          <Text style={styles.text}>You typed...</Text>
+        <View>
+          {goals.map((goal) => {
+            return (
+              <View style={styles.textContainer} key={goal.key}>
+                <Text>{goal.text}</Text>
+              </View>
+            );
+          })}
         </View>
       </View>
       <Input onAdd={onTextAdd} modal={modalVisible} onCancel={onCancel} />
@@ -42,12 +57,12 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     justifyContent: "center",
   },
-  topContainer:{
+  topContainer: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  bottomContainer:{
+  bottomContainer: {
     flex: 4,
     backgroundColor: "pink",
     alignItems: "center",
@@ -55,6 +70,11 @@ const styles = StyleSheet.create({
   textContainer: {
     backgroundColor: "#aaa",
     borderRadius: 5,
-    color:'blue'
+    color: "blue",
+    padding: 30,
+    margin: 30,
   },
+  text:{
+    fontSize:12,
+  }
 });
