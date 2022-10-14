fd408c95ee70aa7dfac3eafa614591911b7ec6d1
SicongZhang <zhang.sico@northeastern.edu>
2022-10-13 23:15:53 -0700

    finish week5

---
+++ components/DeleteButton.js
@@ -0,0 +1,21 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+import React from 'react'
+import {Ionicons} from '@expo/vector-icons';
+
+export default function DeleteButton({onDelete}) {
+  return (
+    <Pressable onPress={onDelete}>
+    <View style={styles.button}>
+      <Ionicons name='trash' size={24} color='black'/>
+    </View>
+    </Pressable>
+  )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        flex:1,
+        fontSize:22,
+        justifyContent:'center',
+    }
+})
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,28 +1,48 @@
 import { View, Text, StyleSheet, Button, Pressable } from "react-native";
 import React from "react";
-
-export default function GoalItem({goal, onDelete, onitemPress}) {
-    
+import DeleteButton from "./DeleteButton";
+export default function GoalItem({ goal, onDelete, onitemPress }) {
+  function deletePressed (){
+   onDelete(goal.key);
+  }
   return (
-    <Pressable onPress={onitemPress}>
-    <View style={styles.textContainer}>
-      <Text>{goal.text}</Text>
-      <Button title="X" onPress={()=>{onDelete(goal.key)}} color='black'/>
-    </View>
-    </Pressable>
+    
+      <View style={styles.textContainer}>
+        <Pressable
+      onPress={onitemPress}
+      style={({pressed}) => {
+        if (pressed) return styles.pressedItem;
+      }}
+    >
+        <Text>{goal.text}</Text>
+        {/* <Button
+          title="X"
+          onPress={() => {
+            onDelete(goal.key);
+          }}
+          color="black"
+        /> */}
+        </Pressable>
+        <DeleteButton onDelete={deletePressed}/>
+      </View>
+    
   );
 }
 const styles = StyleSheet.create({
-    textContainer: {
-        backgroundColor: "#aaa",
-        borderRadius: 5,
-        color: "blue",
-        padding: 30,
-        margin: 30,
-        flexDirection:'row',
-        alignItems:'center',
-    },
-      text: {
-        fontSize: 12,
-      },
-})
+  textContainer: {
+    backgroundColor: "#aaa",
+    borderRadius: 5,
+    padding:5,
+    color: "blue",
+    margin: 30,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 12,
+  },
+  pressedItem: {
+    backgroundColor: "#222",
+    opacity: 0.5,
+  },
+});
